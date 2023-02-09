86a2d283903440c89a5d8d919cf62a0bb658efd9
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-02-09 12:03:59 -0600

    add indicators for dropdowns

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+"use strict"
 // Function to prompt user for name. Returns a string
 function promptForUserName() {
-    let name = prompt('What is your name?');
+    const name = prompt('What is your name?');
     if (name === null) {
         return '';
     } else {
@@ -10,25 +11,29 @@ function promptForUserName() {
 }
 
 // Select DOM elements
-let pageWelcome = document.querySelector('#welcome');
-let sections = document.querySelectorAll('section');
-let headers = document.querySelectorAll('h2');
-let sideNav = document.querySelector('.sidenav');
-let mainSection = document.querySelector('.main')
-let sideNavToggleButton = document.querySelector('.sidenav-toggle');
+const pageWelcome = document.querySelector('#welcome');
+const sections = document.querySelectorAll('section');
+const headers = document.querySelectorAll('h2');
+const sideNav = document.querySelector('.sidenav');
+const mainSection = document.querySelector('.main')
+const sideNavToggleButton = document.querySelector('.sidenav-toggle');
+
 // create welcome heading w/ given userName
-let userName = promptForUserName();
+const userName = promptForUserName();
 pageWelcome.innerHTML = `Hello${userName}, Happy Studying!`;   
 
 // create event listeners for 'collapsible' sections
 sections.forEach(section => { 
     section.addEventListener('click', () => {
-        let content = section.querySelector('.section-content');
+        const content = section.querySelector('.section-content');
+        const arrow = section.querySelector(".right-arrow");
         content.classList.toggle('hide');
         if(content.classList.contains('hide')) {
             content.style.display = 'none';
+            arrow.setAttribute('src', 'https://img.icons8.com/fluency-systems-filled/48/null/right-squared.png');
         } else { 
-            content.style.display = 'block' 
+            content.style.display = 'block';
+            arrow.setAttribute('src', 'https://img.icons8.com/external-those-icons-fill-those-icons/24/null/external-down-arrows-those-icons-fill-those-icons-7.png');
         }
     })
 });
