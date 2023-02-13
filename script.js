10d8821a268d1e1d09a4f3b0edfdeeee5e7ba535
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-02-13 10:42:32 -0600

    add lab 08 requirements

--- script.js
+++ script.js
@@ -1,47 +1,87 @@
-"use strict"
+"use strict";
+
 // Function to prompt user for name. Returns a string
 function promptForUserName() {
-    const name = prompt('What is your name?');
-    if (name === null) {
-        return '';
-    } else {
-        if(name.length < 1) return name;
-        return ' ' + name;
-    }
+  const name = prompt("What is your name?");
+  if (name === null || name.length == 0) {
+    alert("Please enter a valid name");
+    return promptForUserName();
+  } else {
+    return ` ${name}`;
+  }
+}
+function promptForUserNumber() {
+  const imgDiv = document.querySelector(".pic-container")
+  const number = prompt("Please Enter a Number");
+  for (let i = 0; i < number; i++) {
+    let img = document.createElement('img');
+    console.log(img);
+    img.src = "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg";
+    imgDiv.appendChild(img);
+  }
 }
 
 // Select DOM elements
-const pageWelcome = document.querySelector('#welcome');
-const sections = document.querySelectorAll('section');
-const headers = document.querySelectorAll('h2');
-const sideNav = document.querySelector('.sidenav');
-const mainSection = document.querySelector('.main')
-const sideNavToggleButton = document.querySelector('.sidenav-toggle');
+const pageWelcome = document.querySelector("#welcome");
+const sections = document.querySelectorAll("section");
+const headers = document.querySelectorAll("h2");
+const sideNav = document.querySelector(".sidenav");
+const mainSection = document.querySelector(".main");
+const sideNavToggleButton = document.querySelector(".sidenav-toggle");
 
 // create welcome heading w/ given userName
 const userName = promptForUserName();
-pageWelcome.innerHTML = `Hello${userName}, Happy Studying!`;   
+promptForUserNumber();
+pageWelcome.innerHTML = `Hello${userName}, Happy Studying!`;
 
 // create event listeners for 'collapsible' sections
-sections.forEach(section => { 
-    section.addEventListener('click', () => {
-        const content = section.querySelector('.section-content');
-        const arrow = section.querySelector(".right-arrow");
-        content.classList.toggle('hide');
-        if(content.classList.contains('hide')) {
-            content.style.display = 'none';
-            arrow.setAttribute('src', 'https://img.icons8.com/fluency-systems-filled/48/null/right-squared.png');
-        } else { 
-            content.style.display = 'block';
-            arrow.setAttribute('src', 'https://img.icons8.com/external-those-icons-fill-those-icons/24/null/external-down-arrows-those-icons-fill-those-icons-7.png');
-        }
-    })
+sections.forEach((section) => {
+  section.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const content = section.querySelector(".section-content");
+    const arrow = section.querySelector(".right-arrow");
+    content.classList.toggle("hide");
+    if (content.classList.contains("hide")) {
+      content.style.display = "none";
+      arrow.setAttribute(
+        "src",
+        "https://img.icons8.com/fluency-systems-filled/48/null/right-squared.png"
+      );
+    } else {
+      content.style.display = "block";
+      arrow.setAttribute(
+        "src",
+        "https://img.icons8.com/external-those-icons-fill-those-icons/24/null/external-down-arrows-those-icons-fill-those-icons-7.png"
+      );
+    }
+  });
 });
 
-sideNavToggleButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    sideNav.classList.toggle('sidenav');
-    sideNav.classList.toggle('sidenav-left');
-    mainSection.classList.toggle('main-right');
-    mainSection.classList.toggle('main');
-})
+//Wire up side bar button to toggle left and right
+sideNavToggleButton.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sideNav.classList.toggle("sidenav");
+  sideNav.classList.toggle("sidenav-left");
+  mainSection.classList.toggle("main-right");
+  mainSection.classList.toggle("main");
+});
+
+var md = window.markdownit();
+
+function htmlToElements(html) {
+  var template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.childNodes;
+}
+
+const createHtmlFromMd = async () => {
+  let fileData = await fetch("test.md");
+  let markdownData = await fileData.text();
+  let html = md.render(markdownData);
+
+  let elems = htmlToElements(html);
+
+  document.body.append(...elems);
+};
+
+createHtmlFromMd();
